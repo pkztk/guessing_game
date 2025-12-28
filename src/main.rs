2652223a64b5d0f0af9b195bf4b53d8b0429428a
pkztk <pkoziatek@gmail.com>
2025-12-28 16:13:39 +0100

    draft code for guessing game

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,37 @@
+use std::io;
+use std::cmp::Ordering;
+use colored::*;
+
 fn main() {
-    println!("Hello, world!");
+
+    let secret_number = rand::random_range(1..101);
+    println!("The secret number is: {}", secret_number);
+    
+    loop  {
+        println!("Welcome to the Guessing Game! Input your guess:");
+        let mut guess: String = String::new();
+
+        io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Please enter a valid number!");
+                continue;
+            }
+        };
+
+        println!("You guessed: {}", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("{}", "Too small!".red()),
+            Ordering::Greater => println!("{}", "Too big!".red()),
+            Ordering::Equal => {
+                println!("{}", "You win!".green());
+                break;
+            }
+        }
+    }   
 }
-§
